a20103136c6be2d23df044d233ed32a8f3ede502
Heoconsaurang <uyenhuynh13122000@gmail.com>
2023-10-19 21:55:43 +0700

    no message

---
+++ IdeaProjects/HelloJava/src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static void main(String[] args) {
+        String name = "Sam Huynh";
+        int age = 10;
+
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
